69d2fa2b7986a8b2f3dee975f87daea1805241b9
elidarustiya <114423143+elidarustiya@users.noreply.github.com>
2022-10-15 21:19:23 +0700

    Add files via upload

---
+++ Handphone.java
@@ -0,0 +1,31 @@
+/**
+ * Tugas Sesi 2_Pemprograman Berorientasi Objek
+ * @author ACER_ELI DARUSTIYA_20210040068_TI21A
+ */
+
+public class Handphone {
+    
+    String jenis_hp;
+    int tahun_pembuatan;
+
+    public void setDataHP(String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+
+    public String getJenisHP(){
+        return this.jenis_hp;
+    }
+
+    public int getTahnunPembuatan(){
+        return this.tahun_pembuatan;
+    }
+
+    public static void main(String[] args) {
+        Handphone hp = new Handphone();
+        hp.setDataHP("samsung", 2002);
+        System.out.println(hp.getJenisHP());
+        System.out.println(hp.getTahnunPembuatan());
+    }
+    
+}
